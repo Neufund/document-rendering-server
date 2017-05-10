c81387b3ff7ac16dacda16864a564659de826062
mostafabalata <mostafa.balata@gmail.com>
2017-05-10 12:27:19 +0200

    neufund logging format

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ DOWNLOADS_DIR = "downloads"
 CONVERTED_DIR = "converted"
 CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 IPFS_NODE_URL = "https://ipfs.io/ipfs"
+
+LOG_LEVEL='INFO'
+LOG_FORMAT= '{asctime}'#{levelname}#{filename}#{funcName}#{lineno}#{message}
+LOG_COLOR=False
--- server.py
+++ server.py
@@ -4,13 +4,36 @@ from flask import Flask, jsonify, request
 from flask import send_file
 from flask_cors import CORS
 from config import CONVERTED_DIR
+from config import LOG_LEVEL
+from config import LOG_COLOR
+from config import LOG_FORMAT
 app = Flask(__name__)
 app.config.from_mapping(os.environ)
 from tools import *
 
 CORS(app)
 
-logging.basicConfig(level=logging.DEBUG ,format='%(levelname)s:%(message)s')
+# logging.basicConfig(level=logging.DEBUG ,format='%(levelname)s:%(message)s')
+#
+def init_logging():
+    """Initializes logging."""
+    if LOG_COLOR:
+        logging.addLevelName(logging.DEBUG,
+                             "\033[1;00m%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
+        logging.addLevelName(logging.INFO,
+                             "\033[1;34m%s\033[1;0m" % logging.getLevelName(logging.INFO))
+        logging.addLevelName(logging.WARNING,
+                             "\033[0;33m%s\033[0;0m" % logging.getLevelName(logging.WARNING))
+        logging.addLevelName(logging.ERROR,
+                             "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+        logging.addLevelName(logging.CRITICAL,
+                             "\033[0;31m%s\033[0;0m" % logging.getLevelName(logging.CRITICAL))
+
+    logging.basicConfig(level=LOG_LEVEL,
+                        format=LOG_FORMAT, style='{')
+
+
+init_logging()
 
 @app.route('/api/document' , methods=['POST'])
 def replace():
